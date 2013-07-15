831a82031e44d6e52592d5317f5b7c0c19ec9521
Cedric Villa <cedric.villa@gmail.com>
2013-07-15 19:31:53 +0000

    code cleanup

--- manifests/config/commands.pp
+++ manifests/config/commands.pp
@@ -1,14 +1,19 @@
-define mv_nagios::config::commands ($mail_server = 'courrier', $mail_login = '', $mail_password = '') {
+define mv_nagios::config::commands (
+	$mail_server 		= 'courrier', 
+	$mail_login 		= '', 
+	$mail_password 	= ''
+) 
+{
 	require('mv_nagios::packages')
 	require('mv_nagios')
 
-    file {"${mv_nagios::config_path}/commands.cfg": 
-        ensure => file,
-        content => template("mv_nagios/commands.cfg.erb"),
-        mode => "0644",
-        owner => 'root',
-        group => 'root',
+	file {"${mv_nagios::config_path}/commands.cfg": 
+  	ensure 	=> file,
+    content => template("mv_nagios/commands.cfg.erb"),
+    mode 		=> "0644",
+    owner 	=> 'root',
+    group 	=> 'root',
 		require => Package["mv_nagios"],
-        notify => Service["mv_nagios"],
-    }
+    notify 	=> Service["mv_nagios"],
+  }
 }
